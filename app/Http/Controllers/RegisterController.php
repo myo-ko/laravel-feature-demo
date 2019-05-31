46e75afb5995aa4f2a992726a8cca7858ece1f26
Myo Ko Ko <myokoko@megliofuturo.co.jp>
2019-05-31 13:31:39 +0630

    First Commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Cognito\CognitoClient;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Aws\CognitoIdentityProvider\Exception\UserNotFoundException;
+use Aws\CognitoIdentityProvider\Exception\UserNotConfirmedException;
+use Aws\CognitoIdentityProvider\Exception\CognitoIdentityProviderException;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index() {
+        return view('register');
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [    
+            'name' => ['required', 'string', 'max:255'],       
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8'],
+        ]);
+    }
+
+    public function store(Request $request) {
+
+        $validator = $this->validator($request->all());
+        if ($validator->fails())
+        {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $attr = [
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+        ];
+
+        $res = app()->make(CognitoClient::class)->register($request->input('email'), $request->input('password'), $attr);
+
+        return response()->json(true, 200);
+    }
+
+    public function confirm(Request $request) 
+    {
+        $validator = Validator::make($request->all(), [           
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'code' => ['required',],
+        ]);
+
+        if ($validator->fails())
+        {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $attr = [
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+        ];
+        $res = app()->make(CognitoClient::class)->confirmSignUp($request->input('email'), $request->input('code'), $attr);
+        
+        $status = $res['@metadata']['statusCode'];
+        if ($status == 200) {
+            return response()->json("Successfully verified", 200);
+        }else {
+            return response()->json(dd($res), 505);
+        }
+    }
+
+    public function login(Request $request) 
+    {
+        $validator = Validator::make($request->all(), [           
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required',],
+        ]);
+
+        if ($validator->fails())
+        {
+            return response()->json($validator->errors(), 422);
+        }
+
+        try {
+            $result = app()->make(CognitoClient::class)->authenticate($request->input('email'), $request->input('password'));
+
+            $accessToken = $result->get('AuthenticationResult')['AccessToken'];
+    
+            $response = [
+                'status' => true,
+                'token' => $accessToken
+            ];
+            $code = 200;
+        }
+        catch (CognitoIdentityProviderException $e){
+
+            $response = [
+                'status' => false,
+                'error' => $e->getAwsErrorCode(),
+                'message' => $e->getAwsErrorMessage(),
+            ];
+            $code = $e->getStatusCode();
+                       
+        }
+        return response()->json($response, $code);
+
+    }
+}
---
+++ resources/views/register.blade.php
@@ -0,0 +1,112 @@
+<html>
+     <head>
+          <meta charset="UTF-8">
+          <meta name="csrf-token" content="{{ csrf_token() }}" />
+          <title>Register</title>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+     
+          <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+     </head>
+     <body>
+          <div class="container-fluid">
+               <div class="row h-100 justify-content-center align-items-center">
+                    <div class="col-3">
+                         <div class="jumbotron">
+                              <h4 class="display-4">Fill in</h4>
+                              <hr class="my-4">
+                              <form action="" id="inputForm">                             
+                              <div class="form-group">
+                                   <label for="inputEmail">Email address</label>
+                                   <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" value="john@example.com">                              
+                              </div>
+                              <div class="form-group">
+                                   <label for="inputPassword">Password</label>
+                                   <input type="password" class="form-control" id="inputPassword" value="123456789">
+                              </div>
+                              <button type="button" class="btn btn-primary" id="btn-register">Register</button>
+                              <button type="button" class="btn btn-success" id="btn-login">Login</button>
+                              </form>
+                         </div>
+                    </div>
+               </div>
+          </div>
+
+          <script type="text/javascript">
+
+               $('document').ready(function() {
+
+                    function login() 
+                    {
+                         var para = {                         
+                              'email' : $('#inputEmail').val(),
+                              'password' : $('#inputPassword').val()                              
+                         };
+
+                         $.ajax('/login', {
+                              headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                              type: 'POST',
+                              data: para,
+                              dataType: 'json',
+                              success: function(response) {
+                                   console.log(response);
+                                   if (response['status'] == true){
+                                        alert("Login success. See the token in console.");
+                                   }
+                              },
+                              error: function(error) {
+                                   console.log(error.responseJSON);
+                              }
+                         });
+                    }
+
+                    $('#btn-register').on('click', function() {
+
+                         var para = {
+                              'name' : 'Mr.John',
+                              'email' : $('#inputEmail').val(),
+                              'password' : $('#inputPassword').val()                              
+                         };
+
+                         jQuery.ajax(this.href, {
+                              headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                              type: 'POST',
+                              data: para,
+                              dataType: 'JSON',                             
+                              success: function (data) {                                   
+                                   
+                                   var para = {
+                                        'name' : 'Mr.John',
+                                        'email' : $('#inputEmail').val()                                                          
+                                   };
+                                   
+                                   para['code'] = prompt("Verification code", "");
+                                   $.ajax('/confirm', {
+                                        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                                        type: 'POST',
+                                        data: para,
+                                        dataType: 'JSON',                                     
+                                        success: function (data) {
+                                             console.log(data);
+                                        },
+                                        error: function (error) {
+                                             console.log(error);
+                                        }
+                                   });
+                                   
+
+                              },
+                              error: function (error) {
+                                   console.log(error);
+                              }
+                         });
+
+                    });                   
+
+                    $("#btn-login").on('click', login);
+               });               
+               
+          </script>
+     </body>
+</html>
